45eec77e1d33c77ec0a9c77ab9b9da02d2a9a075
Tobias Herbitschek <tobias.herbitschek@gmail.com>
2023-01-15 18:45:39 +0100

    Rename Charactersheet view to Player

---
+++ src/views/Player.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<Hero />
+		<br>
+		<v-container v-if="registered">
+			<CharacterSheet v-bind:character-override="character"></CharacterSheet>
+		</v-container>
+		<v-container v-else>
+			<Login />
+		</v-container>
+	</div>
+</template>
+
+<script>
+import Hero from "@/components/Hero.vue";
+import Login from "@/components/Login.vue";
+
+import CharacterSheet from "../components/CharacterSheet.vue";
+
+export default {
+	name: "Home",
+	components: {
+		Hero,
+		Login,
+		CharacterSheet,
+	},
+	computed: {
+		registered() {
+			return this.$store.state.registered;
+		},
+		character() {
+			return this.$store.state.character[0];
+		}
+	}
+};
+</script>
+
+<style scoped>
+.traits {
+	position: relative;
+}
+</style>
