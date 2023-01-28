30b8116271fcdc4205616322f6f3e84d080ede0c
Tobias Herbitschek <tobias.herbitschek@gmail.com>
2023-01-28 13:02:54 +0100

    Add sample game list

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,16 +1,25 @@
 <template>
+	<v-container>
+		<div v-if="account">
+			<h1>{{ account.nickname }}</h1>
+			<h1>Test</h1>
+		</div>
 
-	<div v-if="account">
-		<h1>{{ account.nickname }}</h1>
-		<h1>Test</h1>
-	</div>
+
+		{{ campaigns }}
+
+		<div v-for="campaign in campaigns" :key="campaign.id">
+			<h1>{{ campaign.name }}</h1>
+			<p>{{ campaign.role }}</p>
+		</div>
+	</v-container>
 </template>
 
 <script>
 import jwtDecode from 'jwt-decode'
 
 export default {
-	data() { return { account: false } },
+	data() { return { account: false, campaigns: [] } },
 	async mounted() {
 		const jwt = localStorage.getItem('jwt');
 
@@ -21,6 +30,12 @@ export default {
 		const response = await this.$axios.get('/users/id/' + decoded.id);
 
 		this.account = response.data;
+
+		const campaignsResponse = await this.$axios.get('/campaigns/my');
+
+		this.campaigns = campaignsResponse.data;
+
+		console.log(this.campaigns);
 	}
 }
 </script>
