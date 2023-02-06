d6982a879b4bcbc18f611b6f3d364c36baec7b8b
k4444th <95476869+k4444th@users.noreply.github.com>
2023-02-06 10:57:15 +0100

    fixed footer colors

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,9 +9,9 @@
                     <v-card-text>
                         <v-list-item>
                             <v-list-item-content>
-                                <v-list-item-title>Thesis: DnD-Table</v-list-item-title>
-                                <v-list-item-subtitle>Livia Löw, Tobias Herbitschek, Katharina Weinberger</v-list-item-subtitle>
-                                <v-list-item-subtitle>© 2022 - 2023</v-list-item-subtitle>
+                                <v-list-item-title class="white--text">Thesis: DnD-Table</v-list-item-title>
+                                <v-list-item-subtitle class="white--text">Livia Löw, Tobias Herbitschek, Katharina Weinberger</v-list-item-subtitle>
+                                <v-list-item-subtitle class="white--text">© 2022 - 2023</v-list-item-subtitle>
                             </v-list-item-content>
                         </v-list-item>
                     </v-card-text>
