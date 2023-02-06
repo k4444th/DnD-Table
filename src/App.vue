c89af6eba02a04ae767b9dac7619936978b05ca9
k4444th <95476869+k4444th@users.noreply.github.com>
2023-02-06 10:54:19 +0100

    added footer

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
     <v-main>
       <router-view />
     </v-main>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 export default {
   name: "App",
   components: {
     Navbar,
+    Footer
   },
 
   data: () => ({ }),
---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <template>
+            <br><br>
+            <br><br>
+            <br><br>
+            <v-footer absolute color="primary">
+                <v-card flat tile width="100%" class="primary text-center">
+                    <v-card-text>
+                        <v-list-item>
+                            <v-list-item-content>
+                                <v-list-item-title>Thesis: DnD-Table</v-list-item-title>
+                                <v-list-item-subtitle>Livia Löw, Tobias Herbitschek, Katharina Weinberger</v-list-item-subtitle>
+                                <v-list-item-subtitle>© 2022 - 2023</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-card-text>
+                </v-card>
+            </v-footer> 
+        </template>
+    </div>
+</template>
