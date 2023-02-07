060ca62245d0f686f70c2de8599e7d3905b33583
k4444th <95476869+k4444th@users.noreply.github.com>
2023-02-07 15:06:50 +0100

    changed home view layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,26 @@
     <PreviewHero/>
     <br>
     <v-container>
-      <WeaponOfTheDay :weapon="wotd" />
+      <h1>Why us?</h1><br>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, quasi ad veniam numquam harum iure autem amet aspernatur incidunt neque omnis quos eum. Quisquam, expedita veritatis sint corrupti quibusdam consequatur?</p>
+      <br><br>
+      <h1>What do we offer?</h1><br>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, quasi ad veniam numquam harum iure autem amet aspernatur incidunt neque omnis quos eum. Quisquam, expedita veritatis sint corrupti quibusdam consequatur?</p>
       <br>
-      <hr>
-      <br>
-      <v-row>
-        <v-col class="col-6 col-md-3 col-lg-2" v-for="weapon in weapons" :key="weapon.name">
-          <Weapon :weapon="weapon"/>
-        </v-col>
-      </v-row>
+        <v-row>
+          <v-col class="col-12 col-md-4" v-for="feature in features" :key="feature.title">
+            <v-card>
+              <v-list-item >
+                <v-list-item-content>
+                  <v-list-item-title>{{ feature.title }}</v-list-item-title>
+                  <v-list-item-subtitle>{{ feature.subtitle }}</v-list-item-subtitle>
+                  <br>
+                  {{ feature.text }}
+                </v-list-item-content>
+              </v-list-item>
+            </v-card>
+          </v-col>
+        </v-row>
     </v-container>
     <br>
   </div>
@@ -20,22 +31,31 @@
 <script>
 // @ is an alias to /src
 import PreviewHero from "@/components/PreviewHero.vue";
-import WeaponOfTheDay from "@/components/WeaponOfTheDay.vue"
-import Weapon from "@/components/Weapon.vue"
 
 export default {
   name: "Home",
   components: {
     PreviewHero,
-    WeaponOfTheDay,
-    Weapon
   },
-  computed: {
-    wotd() {
-      return this.$store.state.weapons[this.$store.state.wotd];
-    },
-    weapons() {
-      return this.$store.state.weapons;
+  data() {
+    return {
+      features: [
+        {
+          title: "Hello 1",
+          subtitle: "Blabla 1",
+          text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, quasi ad veniam numquam harum iure autem amet aspernatur incidunt neque omnis quos eum. Quisquam, expedita veritatis sint corrupti quibusdam consequatur?"
+        },
+        {
+          title: "Hello 2",
+          subtitle: "Blabla 2",
+          text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, quasi ad veniam numquam harum iure autem amet aspernatur incidunt neque omnis quos eum. Quisquam, expedita veritatis sint corrupti quibusdam consequatur?"
+        },
+        {
+          title: "Hello 3",
+          subtitle: "Blabla 3",
+          text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, quasi ad veniam numquam harum iure autem amet aspernatur incidunt neque omnis quos eum. Quisquam, expedita veritatis sint corrupti quibusdam consequatur?"
+        }
+      ]
     }
   }
 };
